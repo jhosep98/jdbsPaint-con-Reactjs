94c982d9effe5f7fd02d482891d1068e930aac8f
Jhosep Davila <jhosepdavila@Jhoseps-MacBook-Air.local>
2019-09-07 23:49:34 -0300

    Updates

---
+++ precache-manifest.7732b6918b15bd13d8d65c89c39b8df1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59c9f11a1591aaed2cc62a26595441e",
+    "url": "/jdbsPaint-con-Reactjs/index.html"
+  },
+  {
+    "revision": "83832b7af51838c17fa1",
+    "url": "/jdbsPaint-con-Reactjs/static/css/main.ac977b26.chunk.css"
+  },
+  {
+    "revision": "804e12fcd49f7ecd14b9",
+    "url": "/jdbsPaint-con-Reactjs/static/js/2.6c69e3cb.chunk.js"
+  },
+  {
+    "revision": "83832b7af51838c17fa1",
+    "url": "/jdbsPaint-con-Reactjs/static/js/main.abbe475d.chunk.js"
+  },
+  {
+    "revision": "d1e2e394f05f08b9a510",
+    "url": "/jdbsPaint-con-Reactjs/static/js/runtime~main.baed0a65.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8795126e71a5e7b1067e35dab840bd06.js"
+  "/jdbsPaint-con-Reactjs/precache-manifest.7732b6918b15bd13d8d65c89c39b8df1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jdbsPaint-con-Reactjs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
